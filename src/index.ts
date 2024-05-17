56322470b70a2c5d082ed23c1378b49958ca8169
gotocityp <57137567+gotocityp@users.noreply.github.com>
2024-05-17 17:09:46 +0800

    optimization
    optimization

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,62 @@
-import childProcess from 'child_process';
-import { type } from 'os';
-import type { Plugin } from "vite";
+import childProcess from 'child_process'
+// import { type } from 'os'
+import type { Plugin } from 'vite'
 
 // 判断平台，win平台不支持grep
-const isWin = type() === 'Windows_NT';
-const findStr = isWin ? 'findstr' : 'grep';
+// const isWin = type() === 'Windows_NT'
+// const findStr = isWin ? 'findstr' : 'grep'
 
 let userName = ''
 
 try {
-	userName = childProcess.execSync(
-		`git config user.name`,
-		{ encoding: 'utf-8' }
-	)
-} catch (e) { }
+  userName = childProcess.execSync(`git config user.name`, { encoding: 'utf-8' })
+} catch (e) {
+  console.log(e)
+}
 
 const VitePluginRmOthersConsole = () => {
-	return {
-		name: 'vite-plugin-rm-others-console',
-		enforce: 'pre',
-		apply: 'serve',
-		transform: (code, id) => {
-			try {
-				// const userName = childProcess.execSync(`git config user.name`, { encoding: 'utf-8' });
+  return {
+    name: 'vite-plugin-rm-others-console',
+    enforce: 'pre',
+    apply: 'serve',
+    transform: async (code, id) => {
+      try {
+        if (!id.includes('node_modules') && id.includes('.ts') && code.includes(`console.log(`) && userName) {
+          const authorList = childProcess
+            .execSync(`git blame ${id} `, {
+              encoding: 'utf-8'
+            })
+            .split('\n')
+          return {
+            code: code
+              .split('\n')
+              .map((row, index) => {
+                const codeInfo = authorList[index]
+                if (
+                  codeInfo.includes('console.log(') &&
+                  !codeInfo.includes(userName) &&
+                  !codeInfo.includes('Not Committed Yet')
+                ) {
+                  return row.replace(/console\.log\((?:[^)(]|\((?:[^)(]+|\([^)(]*\))*\))*\)[;\n]?/g, `{}`)
+                }
+                return row
+              })
+              .join('\n'),
+            map: null // 表示源码视图不作修改
+          }
+        }
+      } catch {
+        return {
+          code,
+          map: null // 表示源码视图不作修改
+        }
+      }
+      return {
+        code,
+        map: null // 表示源码视图不作修改
+      }
+    }
+  } as Plugin
+}
 
-				if (!id.includes('node_modules') && code.includes(`console.log(`) && userName) {
-					const rows = code.split('\n');
-
-					const includesLines = rows.map((row, idx) => (row.includes(`console.log(`) ? idx : undefined)).filter(n => n);
-
-					const removeLine = includesLines.filter((line = 0) => {
-						const authorInfo = childProcess.execSync(
-							`git blame -L ${line + 1},${line + 1} --porcelain ${id} | ${findStr} "^author "`,
-							{ encoding: 'utf-8' }
-						);
-						const author = authorInfo.slice(authorInfo.indexOf(`author `) + 7);
-						// return ![userName, `Not Committed Yet`].includes(author);
-						return !(author.includes(userName) || author.includes('Not Committed Yet'));
-					});
-
-					return {
-						code: rows.map((row, idx) => {
-							if (removeLine.includes(idx)) {
-								return row.replace(/console\.log\((?:[^)(]|\((?:[^)(]+|\([^)(]*\))*\))*\)[;\n]?/g, `{}`);
-							}
-							return row;
-						}).join('\n'),
-						map: null // 表示源码视图不作修改
-					}
-				}
-			} catch {
-				return {
-					code,
-					map: null // 表示源码视图不作修改
-				}
-			}
-			return {
-				code,
-				map: null // 表示源码视图不作修改
-			}
-		},
-	} as Plugin;
-};
-
-export default VitePluginRmOthersConsole;
+export default VitePluginRmOthersConsole
